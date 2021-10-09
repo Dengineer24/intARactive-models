154182a231d16f7e986af0682e8607ba02112dc4
Mielan <37193716+Mielan@users.noreply.github.com>
2021-10-09 12:11:29 -0700

    commented out chatroom & add footer to main

--- Project/interactive/src/containers/Main.tsx
+++ Project/interactive/src/containers/Main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import Home from "../pages/Home/Home"
 import Mechanics from "../pages/Mechanics/Mechanics"
 import About from "../pages/About/About"
-import TwilioRoute from '../pages/Chatroom/TwilioRoute';
+//import TwilioRoute from '../pages/Chatroom/TwilioRoute';
 
 export default function Main() {
     return (
@@ -14,8 +15,11 @@ export default function Main() {
                 <Route path='/' exact component={Home} />
                 <Route path='/mechanics' exact component={Mechanics} />
                 <Route path='/about' exact component={About} />
-                <Route path='/chatroom' exact component={TwilioRoute} />
+                
             </Switch>
+            <Footer />
         </Router>
     )
 }
+
+/* <Route path='/chatroom' exact component={TwilioRoute} /> */
