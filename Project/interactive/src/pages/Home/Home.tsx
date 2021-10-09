4f1a2bdeb78b8605c1217ce052ff7c0a79c0ef0f
Mielan <37193716+Mielan@users.noreply.github.com>
2021-10-08 19:37:38 -0700

    try with tsx

---
+++ Project/interactive/src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home(){
+    return (
+        <>
+            <p className="self-center text-xl tracking-wide text-justify font-sourceSerifPro">
+                IntARactive Models
+            </p>
+        </>
+    )
+}
