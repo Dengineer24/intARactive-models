6de9330b33b5d421747bd1252b531b1e109231cc
Mielan <37193716+Mielan@users.noreply.github.com>
2021-10-09 21:32:50 -0700

    Update App.tsx

--- Project/interactive/src/App.tsx
+++ Project/interactive/src/App.tsx
@@ -2,20 +2,6 @@ import React, { useContext } from "react";
 import Main from "./containers/Main"
 import Loading from "./containers/Loading"
 import ReactDOM from "react-router-dom"
-/*
-function App() {
-
-    return (
-        <>
-            <div className="App">
-
-                <Main />
-
-            </div>
-        </>
-    );
-}
-*/
 
 class App extends React.Component {
     state = {
@@ -45,9 +31,7 @@ class App extends React.Component {
       return (
         <>
             <div className="App">
-
                 <Main />
-
             </div>
         </>
       ); 
